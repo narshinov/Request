515851ecedb2a7efffea3f33eb112680e42b9e93
Nikita Arshinov <nikitaarshinov01k@gmail.com>
2025-04-28 20:24:31 +0300

    Initial commit

---
+++ RequestTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RequestTest
+//
+//  Created by Никита Аршинов on 28.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
